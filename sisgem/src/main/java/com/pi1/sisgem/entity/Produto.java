e25e90b35f6de87179eff71455d59d42fd5d72ac
Gianluca Borelli <gianlucaborelli2@hotmail.com>
2023-04-23 01:33:05 -0300

    Implementado a classe Preco

--- sisgem/src/main/java/com/pi1/sisgem/entity/Produto.java
+++ sisgem/src/main/java/com/pi1/sisgem/entity/Produto.java
@@ -1,50 +1,33 @@
 package com.pi1.sisgem.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Produto {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    public void setId(Long id) {        
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
+    private Long id;    
 
     @Column(nullable = false)
-    private String name;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
+    private String name;    
 
     @Column
-    private Integer Estoque;
-
-    public void setEstoque(Integer estoque) {
-        Estoque = estoque;
-    }
-
-    public Integer getEstoque() {
-        return Estoque;
-    }
+    private Integer Estoque;    
 
     @Column(nullable = true)
     private String Descricao;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Preco> precos = new ArrayList<>();
 
     public void setDescricao(String descricao) {
         Descricao = descricao;
@@ -54,5 +37,28 @@ public class Produto {
         return Descricao;
     }
 
+    public void setEstoque(Integer estoque) {
+        Estoque = estoque;
+    }
+
+    public Integer getEstoque() {
+        return Estoque;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {        
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
     
 }
