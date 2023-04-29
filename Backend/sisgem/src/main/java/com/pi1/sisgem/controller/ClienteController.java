cef051dfa6194a8d3594297eafcc4938ddc2dbdf
Gianluca Borelli <gianlucaborelli2@hotmail.com>
2023-04-29 00:47:27 -0300

    implementado findByName

--- Backend/sisgem/src/main/java/com/pi1/sisgem/controller/ClienteController.java
+++ Backend/sisgem/src/main/java/com/pi1/sisgem/controller/ClienteController.java
@@ -3,6 +3,8 @@ package com.pi1.sisgem.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pi1.sisgem.data.ClienteRepositorio;
@@ -25,6 +28,11 @@ public class ClienteController {
     @GetMapping
     public List<Cliente> listar(){
         return repositorio.findAll();
+    }
+
+    @GetMapping("/findByName")
+    public ResponseEntity<List<Cliente>> findbyName(@RequestParam("name") String name){
+        return new ResponseEntity<List<Cliente>>(repositorio.findByNameContaining(name), HttpStatus.OK ) ;
     }
 
     @PostMapping
--- Backend/sisgem/src/main/java/com/pi1/sisgem/entity/Cliente.java
+++ Backend/sisgem/src/main/java/com/pi1/sisgem/entity/Cliente.java
@@ -1,12 +1,12 @@
 package com.pi1.sisgem.entity;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,14 +14,14 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 
 @Entity
-public class Cliente {
+public class Cliente implements Serializable {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private String nome;
+    private String name;
     @Column(nullable = false)
     private String obs;
     @OneToMany(cascade = CascadeType.ALL)
@@ -45,11 +45,11 @@ public class Cliente {
         this.id = id;
     }
 
-    public String getNome() {
-        return nome;
+    public String getName() {
+        return name;
     }
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getObs() {
