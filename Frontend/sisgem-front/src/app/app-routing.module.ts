25a1bf91523366bfd358da9eea5f0160c36243ff
Jose <lucas.zambianco@outlook.com>
2023-05-22 22:25:16 -0300

    novo-orcamento_alteraçao_2

--- Frontend/sisgem-front/src/app/app-routing.module.ts
+++ Frontend/sisgem-front/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/views/home/home.component';
 import { OrcamentoReadComponent } from './components/views/orcamento/orcamento-read/orcamento-read.component';
@@ -6,6 +6,7 @@ import { ClientesComponent } from './components/views/clientes/clientes.componen
 import { ProdutosComponent } from './components/views/produtos/produtos.component';
 import { CadastroprodutosComponent } from './components/views/produtos/cadastroprodutos/cadastroprodutos.component';
 import { CadastroclientesComponent } from './components/views/clientes/cadastroclientes/cadastroclientes.component';
+import { NovoOrcamentoComponent } from './components/views/orcamento/novo-orcamento/novo-orcamento.component';
 
 
 const routes: Routes = [
@@ -23,10 +24,12 @@ const routes: Routes = [
     path: 'orcamentos',
     component: OrcamentoReadComponent
   },
+
   {
     path: 'produtos',
     component: ProdutosComponent
   },
+
   {
     path: 'cadastroprodutos',
     component: CadastroprodutosComponent
