b288ca63cc35d438e2ac74359e5cf486c01e8e5c
Gianluca Borelli <gianlucaborelli2@hotmail.com>
2023-05-25 22:11:17 -0300

    Pequena correção em orçamentos

--- Frontend/sisgem-front/src/app/components/views/orcamento/orcamento-read/orcamento-read.component.ts
+++ Frontend/sisgem-front/src/app/components/views/orcamento/orcamento-read/orcamento-read.component.ts
@@ -1,152 +1,261 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject } from "@angular/core";
 import {
   MAT_MOMENT_DATE_FORMATS,
   MomentDateAdapter,
   MAT_MOMENT_DATE_ADAPTER_OPTIONS,
-} from '@angular/material-moment-adapter';
-import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
-import { FormGroup, FormControl } from '@angular/forms';
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import 'moment/locale/pt';
-
+} from "@angular/material-moment-adapter";
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+} from "@angular/material/core";
+import { FormGroup, FormControl } from "@angular/forms";
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
+import "moment/locale/pt";
 
 @Component({
-  selector: 'app-orcamento',
-  styleUrls: ['./orcamento-read.component.css'],
-  templateUrl: './orcamento-read.component.html',
+  selector: "app-orcamento",
+  styleUrls: ["./orcamento-read.component.css"],
+  templateUrl: "./orcamento-read.component.html",
   animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    trigger("detailExpand", [
+      state("collapsed", style({ height: "0px", minHeight: "0" })),
+      state("expanded", style({ height: "*" })),
+      transition(
+        "expanded <=> collapsed",
+        animate("225ms cubic-bezier(0.4, 0.0, 0.2, 1)")
+      ),
     ]),
-  ],  
-  providers: [    
-    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'},    
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: "pt-BR" },
     {
       provide: DateAdapter,
       useClass: MomentDateAdapter,
       deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
     },
-    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
   ],
 })
-
-
 export class OrcamentoReadComponent {
-
   constructor(
     private _adapter: DateAdapter<any>,
-    @Inject(MAT_DATE_LOCALE) private _locale: string,
+    @Inject(MAT_DATE_LOCALE) private _locale: string
   ) {}
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  
+  displayedColumns: string[] = ["cliente", "valorTotal", "dataInicio", "dataFim"];
+
   dataSource = ELEMENT_DATA;
-  columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
-  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
-  expandedElement: PeriodicElement | null = null;  
+  columnsToDisplay = ["name", "weight", "symbol", "position"];
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, "expand"];
+  expandedElement: ProdutoPedido | null = null;
   range = new FormGroup({
     start: new FormControl<Date | null>(null),
     end: new FormControl<Date | null>(null),
-  });  
+  });
 }
 
-export interface PeriodicElement {
+export interface Orcamento {
+  id: number;
+  dataInicio: string;
+  dataFim: string;
+  valorTotal: number;
+  cliente: Cliente;
+  endereco: Endereco;
+  produtoPedidos: ProdutoPedido[];
+}
+
+export interface Cliente {
+  id: number;
+  nome: string;
+}
+
+export interface Endereco {
+  id: number;
+  logradouro: string;
+  numero: number;
+  cidade: string;
+  estado: string;
+}
+
+export interface ProdutoPedido {
+  id: number;
+  quantidade: number;
+  preco: number;
+  produto: Produto;
+}
+
+export interface Produto {
+  id: number;
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-  description: string;
+  estoque: number;
+  descricao: string;
+  precos: number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
+const ELEMENT_DATA: Orcamento[] = [
   {
-    position: 1,
-    name: 'Hydrogen',
-    weight: 1.0079,
-    symbol: 'H',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`,
+    id: 1,
+    dataInicio: "2023-01-01",
+    dataFim: "2023-01-10",
+    valorTotal: 100.0,
+    cliente: {
+      id: 1,
+      nome: "Cliente 1",
+    },
+    endereco: {
+      id: 1,
+      logradouro: "Rua A",
+      numero: 10,
+      cidade: "Cidade 1",
+      estado: "Estado 1",
+    },
+    produtoPedidos: [
+      {
+        id: 1,
+        quantidade: 2,
+        preco: 20.0,
+        produto: {
+          id: 1,
+          name: "Produto 1",
+          estoque: 10,
+          descricao: "Descrição do Produto 1",
+          precos: 10.0,
+        },
+      },
+      {
+        id: 2,
+        quantidade: 3,
+        preco: 30.0,
+        produto: {
+          id: 2,
+          name: "Produto 2",
+          estoque: 15,
+          descricao: "Descrição do Produto 2",
+          precos: 12.0,
+        },
+      },
+      {
+        id: 3,
+        quantidade: 1,
+        preco: 15.0,
+        produto: {
+          id: 3,
+          name: "Produto 3",
+          estoque: 8,
+          descricao: "Descrição do Produto 3",
+          precos: 15.0,
+        },
+      },
+      {
+        id: 4,
+        quantidade: 4,
+        preco: 40.0,
+        produto: {
+          id: 4,
+          name: "Produto 4",
+          estoque: 20,
+          descricao: "Descrição do Produto 4",
+          precos: 10.0,
+        },
+      },
+      {
+        id: 5,
+        quantidade: 2,
+        preco: 24.0,
+        produto: {
+          id: 5,
+          name: "Produto 5",
+          estoque: 12,
+          descricao: "Descrição do Produto 5",
+          precos: 12.0,
+        },
+      },
+    ],
   },
   {
-    position: 2,
-    name: 'Helium',
-    weight: 4.0026,
-    symbol: 'He',
-    description: `Helium is a chemical element with symbol He and atomic number 2. It is a
-        colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
-        group in the periodic table. Its boiling point is the lowest among all the elements.`,
-  },
-  {
-    position: 3,
-    name: 'Lithium',
-    weight: 6.941,
-    symbol: 'Li',
-    description: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
-        silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
-        lightest solid element.`,
-  },
-  {
-    position: 4,
-    name: 'Beryllium',
-    weight: 9.0122,
-    symbol: 'Be',
-    description: `Beryllium is a chemical element with symbol Be and atomic number 4. It is a
-        relatively rare element in the universe, usually occurring as a product of the spallation of
-        larger atomic nuclei that have collided with cosmic rays.`,
-  },
-  {
-    position: 5,
-    name: 'Boron',
-    weight: 10.811,
-    symbol: 'B',
-    description: `Boron is a chemical element with symbol B and atomic number 5. Produced entirely
-        by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a
-        low-abundance element in the Solar system and in the Earth's crust.`,
-  },
-  {
-    position: 6,
-    name: 'Carbon',
-    weight: 12.0107,
-    symbol: 'C',
-    description: `Carbon is a chemical element with symbol C and atomic number 6. It is nonmetallic
-        and tetravalent—making four electrons available to form covalent chemical bonds. It belongs
-        to group 14 of the periodic table.`,
-  },
-  {
-    position: 7,
-    name: 'Nitrogen',
-    weight: 14.0067,
-    symbol: 'N',
-    description: `Nitrogen is a chemical element with symbol N and atomic number 7. It was first
-        discovered and isolated by Scottish physician Daniel Rutherford in 1772.`,
-  },
-  {
-    position: 8,
-    name: 'Oxygen',
-    weight: 15.9994,
-    symbol: 'O',
-    description: `Oxygen is a chemical element with symbol O and atomic number 8. It is a member of
-         the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
-         agent that readily forms oxides with most elements as well as with other compounds.`,
-  },
-  {
-    position: 9,
-    name: 'Fluorine',
-    weight: 18.9984,
-    symbol: 'F',
-    description: `Fluorine is a chemical element with symbol F and atomic number 9. It is the
-        lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard
-        conditions.`,
-  },
-  {
-    position: 10,
-    name: 'Neon',
-    weight: 20.1797,
-    symbol: 'Ne',
-    description: `Neon is a chemical element with symbol Ne and atomic number 10. It is a noble gas.
-        Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about
-        two-thirds the density of air.`,
+    id: 2,
+    dataInicio: "2023-02-01",
+    dataFim: "2023-02-15",
+    valorTotal: 150.0,
+    cliente: {
+      id: 2,
+      nome: "Cliente 2",
+    },
+    endereco: {
+      id: 2,
+      logradouro: "Rua B",
+      numero: 20,
+      cidade: "Cidade 2",
+      estado: "Estado 2",
+    },
+    produtoPedidos: [
+      {
+        id: 6,
+        quantidade: 3,
+        preco: 45.0,
+        produto: {
+          id: 6,
+          name: "Produto 6",
+          estoque: 18,
+          descricao: "Descrição do Produto 6",
+          precos: 15.0,
+        },
+      },
+      {
+        id: 7,
+        quantidade: 2,
+        preco: 30.0,
+        produto: {
+          id: 7,
+          name: "Produto 7",
+          estoque: 10,
+          descricao: "Descrição do Produto 7",
+          precos: 15.0,
+        },
+      },
+      {
+        id: 8,
+        quantidade: 4,
+        preco: 40.0,
+        produto: {
+          id: 8,
+          name: "Produto 8",
+          estoque: 16,
+          descricao: "Descrição do Produto 8",
+          precos: 10.0,
+        },
+      },
+      {
+        id: 9,
+        quantidade: 1,
+        preco: 12.0,
+        produto: {
+          id: 9,
+          name: "Produto 9",
+          estoque: 6,
+          descricao: "Descrição do Produto 9",
+          precos: 12.0,
+        },
+      },
+      {
+        id: 10,
+        quantidade: 2,
+        preco: 24.0,
+        produto: {
+          id: 10,
+          name: "Produto 10",
+          estoque: 12,
+          descricao: "Descrição do Produto 10",
+          precos: 12.0,
+        },
+      },
+    ],
   },
 ];
