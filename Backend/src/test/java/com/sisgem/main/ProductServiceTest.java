424e45118e8ef32df90d393b4c43f8033ad29d84
maurovicentedasilva <maurovicente.silva@gmail.com>
2024-05-05 09:38:20 -0300

    Inclusão testes unitários da classe ProductService

---
+++ Backend/src/test/java/com/sisgem/main/ProductServiceTest.java
@@ -0,0 +1,95 @@
+package com.sisgem.main;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.sisgem.main.product.Product;
+import com.sisgem.main.product.ProductRepository;
+import com.sisgem.main.product.ProductService;
+import com.sisgem.main.product.dto.ProductStockDto;
+import com.sisgem.main.product.exceptions.InsufficientStockException;
+import com.sisgem.main.product.exceptions.ProductNotFoundException;
+import com.sisgem.main.quotation.Quotation;
+import com.sisgem.main.quotation.repository.QuotationRepository;
+
+public class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+    
+    @Mock
+    private QuotationRepository quotationRepository;
+
+    @InjectMocks
+    private ProductService productService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testFindById() {
+        UUID productId = UUID.randomUUID();
+        Product product = new Product();
+        product.setId(productId);
+
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+
+        assertEquals(product, productService.findById(productId));
+    }
+
+    @Test
+    public void testFindById_ProductNotFound() {
+        UUID productId = UUID.randomUUID();
+
+        when(productRepository.findById(productId)).thenReturn(Optional.empty());
+
+        assertThrows(ProductNotFoundException.class, () -> productService.findById(productId));
+    }
+
+    @Test
+    public void testGetAvailableProducts() {
+        Date initialDate = new Date();
+        Date finalDate = new Date();
+        List<Product> products = new ArrayList<>();
+        List<Quotation> quotations = new ArrayList<>();
+
+        when(productRepository.findAll()).thenReturn(products);
+        when(quotationRepository.findByIntervalOfDates(initialDate, finalDate)).thenReturn(quotations);
+
+        
+    }
+
+    /*    Teste abaixo, desativado... erro ainda pendente (Mauro).
+    @Test
+    void testStockAvailable_InsufficientStock() {
+        Date initialDate = new Date();
+        Date finalDate = new Date();
+        UUID productId = UUID.randomUUID();
+        int amount = 10;
+        List<ProductStockDto> availableProductsList = new ArrayList<>();
+
+        // Populate availableProductsList
+
+        when(productService.getAvailableProducts(initialDate, finalDate)).thenReturn(availableProductsList);
+
+        assertThrows(InsufficientStockException.class, () -> productService.stockAvailable(initialDate, finalDate, amount, productId));
+    }
+    */
+
+
+
+}
